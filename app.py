6bf711efb6e89cacfd4903149789401a0a21f7d4
Nayan <cnayan789@gmail.com>
2023-07-23 11:12:34 +0545

    sqlite connection established

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import eel
+import sqlite3
 
+conn = sqlite3.connect("userdata.db")
 
-eel.init("web")
-eel.start("index.html")
+cursor = conn.cursor()
+
+cursor.execute(
+  '''CREATE TABLE userdata(
+      file_location text,
+      web_url text,
+      image_path text,
+      image blob
+  )'''
+)
+
+conn.commit()
+
+# eel.init("web") 
+# eel.start("index.html") 
